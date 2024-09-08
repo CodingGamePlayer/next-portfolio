d17b545e7a28381accf399c4e02345e65c49c0cb
kim.jongsik <otw1917@naver.com>
2024-09-09 02:38:42 +0900

    fix: build 버그 수정

--- app/components/Calendar.tsx
+++ app/components/Calendar.tsx
@@ -17,7 +17,7 @@ const Calendar: React.FC<CalendarProps> = ({
     const firstDay = new Date(year, month, 1);
     const lastDay = new Date(year, month + 1, 0);
 
-    let rows = [];
+    const rows = [];
     let days = [];
     let day = 1;
 
